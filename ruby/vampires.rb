785142b38a8ceb22b9f3f04feee84e6644d3b59a
Bradley Tamura <bradley.tamura@gmail.com>
2017-01-24 20:35:34 -0500

    Add questions to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name, please?"
+users_name = gets.chomp
+
+puts "How old are you?"
+users_age = gets.chomp.to_i
+
+puts "What year were you born?"
+users_birth_year = gets.chomp.to_i
+
+puts "Our company cafeteria serves garlic bread.  Should we order some for you?"
+more_garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
