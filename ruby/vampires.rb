bae188cfdf44022ed119149c37e636dc63f01043
Bradley Tamura <bradley.tamura@gmail.com>
2017-01-26 21:59:59 -0500

    Delete p's and replace with puts

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,36 +1,37 @@
-puts "How many employees will be processed? "
+print "How many employees will be processed? "
 
 employees_processed = gets.chomp.to_i
 
+puts employees_processed
     
 until employees_processed == 0 
 
-  puts ""
+
   print "What is your name, please? "
     users_name = gets.chomp
-    p users_name
+    puts users_name
      
-  puts ""               
+              
   print "How old are you? "
     users_age = gets.chomp.to_i
-    p users_age
+    puts users_age
 
-  puts ""
+
   print "What year were you born? "
     users_birth_year = gets.chomp.to_i
-    p users_birth_year
+    puts users_birth_year
 
   age_calculated = 2017 - users_birth_year
 
-  puts ""
+
   print "Our company cafeteria serves garlic bread.  Should we order some for you? y/n. "
     more_garlic = gets.chomp
-    p more_garlic
+    puts more_garlic
 
-  puts ""
+
   print "Would you like to enroll in the company's health insurance? y/n. "
     health_insurance = gets.chomp
-    p health_insurance
+    puts health_insurance
 
   allergies = ""
     
@@ -70,6 +71,6 @@ until employees_processed == 0
 #This second end is ending the until loop
 end
 
-puts ""
+
 print "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
