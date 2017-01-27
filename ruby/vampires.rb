fca5ff4bb6e9eb379737bb6def154f06cb9808dd
Bradley Tamura <bradley.tamura@gmail.com>
2017-01-26 19:44:06 -0500

    Fix if elsif order

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,34 +1,36 @@
 puts "How many employees will be processed?"
 
 employees_processed = gets.chomp.to_i
-p employees_processed
+print employees_processed
     
 until employees_processed == 0 
 
-    employees_processed -= 1
-    
-    puts "What is your name, please?"
+    puts ""
+    print "What is your name, please?"
         users_name = gets.chomp
         p users_name
-
-    puts "How old are you?"
+                    
+    print "How old are you?"
         users_age = gets.chomp.to_i
         p users_age
 
-    puts "What year were you born?"
+    print "What year were you born?"
         users_birth_year = gets.chomp.to_i
         p users_birth_year
 
-    puts "Our company cafeteria serves garlic bread.  Should we order some for you? y/n"
+        age_calculated = 2017 - users_birth_year
+
+    print "Our company cafeteria serves garlic bread.  Should we order some for you? y/n"
         more_garlic = gets.chomp
         p more_garlic
 
-    puts "Would you like to enroll in the company's health insurance? y/n"
+    print "Would you like to enroll in the company's health insurance? y/n"
         health_insurance = gets.chomp
         p health_insurance
 
     loop do
-    puts "What are your allergies? When finished type in \"done\" "
+        print "What are your allergies? When finished type in \"done\" "
+        
         users_allergies = gets.chomp
     
       if users_allergies != "sunshine"
@@ -43,24 +45,27 @@ until employees_processed == 0
 
 
 
-    if users_age + users_birth_year == 2017 && (more_garlic == "y" || health_insurance == "y")
-        puts "Probably NOT a vampire."
+    if (users_name == "Drake Cula") || (users_name == "Tu Fang")
+    puts "Definitely a vampire."
+
+    elsif (users_age == age_calculated) && (more_garlic == "y" || health_insurance == "y")
+    puts "Probably NOT a vampire."
     
-    elsif users_age + users_birth_year != 2017 && (more_garlic == "n" || health_insurance == "n")
-        puts "Probably a vampire."
+    elsif (users_age != age_calculated) && (more_garlic == "n" && health_insurance == "n")
+    puts "Almost certainly a vampire."
     
-    elsif users_age + users_birth_year != 2017 && (more_garlic == "n" && health_insurance == "n")
-        puts "Almost certainly a vampire."
-    
-    elsif users_name == "Drake Cula" || users_name == "Tu Fang"
-        puts "Definitely a vampire."
+    elsif (users_age != age_calculated) && (more_garlic == "n" || health_insurance == "n")
+    puts "Probably a vampire."
     
     else
-        puts "Results inconclusive."
+    puts "Results inconclusive."
+    
     end
 
+    employees_processed -= 1
 #This second end is ending the until loop
 end
 
 
 print "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+
